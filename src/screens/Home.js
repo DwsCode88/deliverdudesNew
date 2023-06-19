7f5ef56ccd691596bf2c0db03d34de3a3cdf39eb
DwsCode88 <42653240+DwsCode88@users.noreply.github.com>
2023-06-18 23:47:49 -0400

    Seachbar

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HeaderTabs from "../../components/HeaderTabs";
+import SearchBar from "../../components/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={{ backgroundColor: "lightgrey", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </SafeAreaView>
   );
