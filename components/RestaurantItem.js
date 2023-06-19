aab4bb7d67aacceda4e46a087c17db5de3b84ae3
DwsCode88 <42653240+DwsCode88@users.noreply.github.com>
2023-06-19 02:34:29 -0400

    updates and assets

---
+++ components/RestaurantItem.js
@@ -0,0 +1,95 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const localRestaurants = [
+  {
+    name: "Beachside Bar",
+    image_url:
+      "https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: "Benihana",
+    image_url:
+      "https://images.unsplash.com/photo-1592861956120-e524fc739696?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 3.7,
+  },
+  {
+    name: "India's Grill",
+    image_url:
+      "https://plus.unsplash.com/premium_photo-1673830185613-fba8baacca0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80",
+    categories: ["Indian", "Bar"],
+    price: "$$",
+    reviews: 700,
+    rating: 4.9,
+  },
+];
+
+export default function RestaurantItem() {
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
+      {localRestaurants.map((restaurant, index) => (
+        <View
+          key={index}
+          style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
+        >
+          <RestaurantImage image={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
+    </TouchableOpacity>
+  );
+}
+
+const RestaurantImage = (props) => (
+  <>
+    <Image
+      source={{
+        uri: props.image,
+      }}
+      style={{ width: "100%", height: 180 }}
+    />
+    <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="black" />
+    </TouchableOpacity>
+  </>
+);
+
+const RestaurantInfo = (props) => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
+      <Text style={{ fontSize: 13, color: "grey" }}>30-45 • min</Text>
+      <Text style={{ fontSize: 13, color: "grey" }}>
+        Thai, Asian Fusion, Noodles, Vegetarian Friendly
+      </Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: "#eee",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        borderRadius: 15,
+        justifyContent: "center",
+      }}
+    >
+      <Text>{props.rating}</Text>
+    </View>
+  </View>
+);
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HeaderTabs from "../../components/HeaderTabs";
 import SearchBar from "../../components/SearchBar";
+import Categories from "../../components/Categories";
+import RestaurantItem from "../../components/RestaurantItem";
 
 export default function Home() {
   return (
@@ -11,6 +13,10 @@ export default function Home() {
         <HeaderTabs />
         <SearchBar />
       </View>
+      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <RestaurantItem />
+      </ScrollView>
     </SafeAreaView>
   );
 }
