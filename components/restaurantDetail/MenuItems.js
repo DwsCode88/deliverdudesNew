7d1a9b0a2abe4b5fcc2699e07799ab6b7843bc0a
DwsCode88 <42653240+DwsCode88@users.noreply.github.com>
2023-06-25 00:08:17 -0400

    scrollview

---
+++ components/restaurantDetail/MenuItems.js
@@ -0,0 +1,94 @@
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import React from "react";
+
+const foods = [
+  {
+    title: "Lasagna",
+    description: "With butter lettuce, tomato and sauce bechamel",
+    price: "$13.50",
+    image:
+      "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
+  },
+
+  {
+    title: "Tandoori Chicken",
+    description:
+      "Amazing Indian dish with tenderloin chicken off the sizzles 🔥",
+    price: "$19.20",
+    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
+  },
+  {
+    title: "Chilaquiles",
+    description:
+      "Chilaquiles with cheese and sauce. A delicious mexican dish 🇲🇽",
+    price: "$14.50",
+    image:
+      "https://drivemehungry.com/wp-content/uploads/2022/07/strawberry-cheesecake-11.jpg",
+  },
+  {
+    title: "Chicken Caesar Salad",
+    description: "One can never go wrong with a chicken caesar salad.",
+    price: "$21.50",
+    image:
+      "https://sugarspunrun.com/wp-content/uploads/2019/01/Best-Cheesecake-Recipe-2-1-of-1-7.jpg",
+  },
+  {
+    title: "CheeseCake with strawberry",
+    description: "One can never go wrong with a chicken caesar salad.",
+    price: "$21.50",
+    image:
+      "https://sugarspunrun.com/wp-content/uploads/2019/01/Best-Cheesecake-Recipe-2-1-of-1-7.jpg",
+  },
+];
+
+const styles = StyleSheet.create({
+  menuItemStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20,
+  },
+  titleStyle: {
+    fontSize: 19,
+    fontWeight: "600",
+  },
+  descriptionStyle: {
+    fontSize: 16,
+    color: "#888",
+  },
+  priceStyle: {
+    fontSize: 16,
+    color: "#888",
+  },
+});
+
+export default function MenuItems() {
+  return (
+    <ScrollView>
+      {foods.map((food, index) => (
+        <View key={index}>
+          <View style={styles.menuItemStyle}>
+            <FoodInfo foods={food} />
+            <FoodImage foods={food} />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const FoodInfo = (props) => (
+  <View style={{ width: 240, justifyContent: "space-evenly" }}>
+    <Text style={styles.titleStyle}>{props.foods.title}</Text>
+    <Text style={styles.descriptionStyle}>{props.foods.description}</Text>
+    <Text style={styles.priceStyle}>{props.foods.price}</Text>
+  </View>
+);
+
+const FoodImage = (props) => (
+  <View>
+    <Image
+      source={{ uri: props.foods.image }}
+      style={{ width: 100, height: 100 }}
+    />
+  </View>
+);
